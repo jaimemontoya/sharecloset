c7ec508f1de4410c99515e15aaf91c72952e348d
Jaime Montoya <jaimemontoya@Jaimes-MacBook-Pro.local>
2015-10-31 13:54:10 -0400

    Test a .py file, which will eventually contain CRUD functions to Create, Read, Update, and Delete.

---
+++ www/cgi-bin/crud.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# prints a minimal HTTP header
+print 'Content-Type: text/html'
+print
+print '<html>'
+print '	<head>'
+print '		<title>'
+print '''
+			CRUD
+		</title>
+	</head>
+'''
+print '<body>'
+print '		<h1>CRUD</h1>'
+print '''
+	</body>
+</html>'''

629cdc543a56811329912352c82df6b975eaaa11
April Uzzle <april.uzzle@rochester.edu>
2015-11-01 20:46:54 -0500

    Update crud.py

--- www/cgi-bin/crud.py
+++ www/cgi-bin/crud.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 # print the http header
-print "Content-Type: text/html"
-print # don't forget the extra newline
+# print "Content-Type: text/html"
+# print # don't forget the extra newline
+
+# import cgi
+# form = cgi.FieldStorage()
+# usernameValue = form['usernameValue'].value
+# import json
+# data = {}
+# data['myUsername'] = usernameValue
+# print json.dumps(data)
+import cgitb
+cgitb.enable()
+
 
 import cgi
 form = cgi.FieldStorage()
-usernameValue = form['usernameValue'].value
-import json
-data = {}
-data['myUsername'] = usernameValue
-print json.dumps(data)
+
+user_name = form['user_name'].value
+user_password = form['user_password'].value
+user_zip = form['user_zip'].value
+
+# insert new user data into the database
+import sqlite3
+
+conn = sqlite3.connect('donors.db')
+c = conn.cursor()
+
+c.execute('insert into users values (?, ?, ?)', (user_name, user_password, user_zip))
+conn.commit()
+conn.close()

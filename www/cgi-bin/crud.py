2b63ccb895cb20d68c74c15f859458307a056ee5
April Uzzle <april.uzzle@rochester.edu>
2015-11-01 21:14:25 -0500

    Update crud.py

--- www/cgi-bin/crud.py
+++ www/cgi-bin/crud.py
@@ -3,23 +3,25 @@
 # print "Content-Type: text/html"
 # print # don't forget the extra newline
 
-# import cgi
-# form = cgi.FieldStorage()
+import cgi
+form = cgi.FieldStorage()
 # usernameValue = form['usernameValue'].value
-# import json
-# data = {}
-# data['myUsername'] = usernameValue
-# print json.dumps(data)
+
+
 import cgitb
 cgitb.enable()
 
 
-import cgi
-form = cgi.FieldStorage()
+usernameValue = form['user_name'].value
+passwordValue = form['user_password'].value
+zipcodeValue = form['user_zip'].value
 
-user_name = form['user_name'].value
-user_password = form['user_password'].value
-user_zip = form['user_zip'].value
+import json
+data = {}
+data['myUsername'] = usernameValue
+data['myPassword'] = passwordValue
+data['myZipcode'] = zipcodeValue
+print json.dumps(data)
 
 # insert new user data into the database
 import sqlite3

1beca19be3443257e714afe6b2bc127d238554ff
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-13 14:44:19 -0500

    Database structure updates
    Database structure updates to associate items to donors and organizations.

--- www/cgi-bin/create-usersdb.py
+++ www/cgi-bin/create-usersdb.py
@@ -19,15 +19,44 @@ c.execute("insert into users values('bobram8', '92798@!', 14043);")
 
 #table for items called 'items' inside donors.db
 #note: no primary key because allowing multiple entries with the same username. 
-c.execute('create table items(username varchar(10), type varchar(30), subtype varchar(30), quantity int)')
+#c.execute('create table items(username varchar(10), type varchar(30), subtype varchar(30), quantity int)')
 
 #filling in with a few items
-c.execute("insert into items values('stargirl27','clothing','jeans',5);")
-c.execute("insert into items values('emichel2','clothing','coats',2);")
-c.execute("insert into items values('fish99','appliances','mixer',1);")
-c.execute("insert into items values('bobram8','clothing','shoes',3);")
-c.execute("insert into items values('stargirl27','toys','dress-up clothes',1);")
-c.execute("insert into items values('bobram8','furniture','couch',1);")
+#c.execute("insert into items values('stargirl27','clothing','jeans',5);")
+#c.execute("insert into items values('emichel2','clothing','coats',2);")
+#c.execute("insert into items values('fish99','appliances','mixer',1);")
+#c.execute("insert into items values('bobram8','clothing','shoes',3);")
+#c.execute("insert into items values('stargirl27','toys','dress-up clothes',1);")
+#c.execute("insert into items values('bobram8','furniture','couch',1);")
+
+# Creating a table called 'item'.
+c.execute('CREATE TABLE item(itemid integer primary key, itemname varchar(30), itemtypeid integer)')
+# Populating the table 'item'
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (1, "Shirt", 1)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (2, "Pants", 1)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES(3, "Shoes", 1)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (4, "Bed", 2)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (5, "Table", 2)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (6, "Desk", 2)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (7, "Board game", 3)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (8, "Video game", 3)')
+c.execute('INSERT INTO item(itemid, itemname, itemtypeid) VALUES (9, "Doll", 3)')
+
+# Creating a table called 'type'
+c.execute('CREATE TABLE type(typeid integer primary key, typename varchar(20))')
+# Populating the table 'type'
+c.execute('INSERT INTO type(typeid, typename) VALUES (1, "Clothes")')
+c.execute('INSERT INTO type(typeid, typename) VALUES (2, "Furniture")')
+c.execute('INSERT INTO type(typeid, typename) VALUES (3, "Toys")')
+
+# Creating a table called 'user_donation'
+c.execute('CREATE TABLE user_donation(user_donationsid integer primary key autoincrement, username varchar(10), itemid integer, quantity integer, timestamp not null default current_timestamp)')
+
+# Creating a table called 'organization'
+c.execute('CREATE TABLE organization(organizationid integer primary key, organizationname varchar(30), zipcode integer)')	
+
+# Creating a table called 'item_organization'
+c.execute('CREATE TABLE item_organization(organizationid integer primary key, organizationname varchar(30), zipcode integer)')
 
 conn.commit()
 conn.close()

1c10709c1fda99b8aa85c024f85be9845bca73c7
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-13 16:18:00 -0500

    Returning types from the database
    Returning types from the database to populate the select option boxes for the types, when users donate items.

--- www/cgi-bin/gettypes.py
+++ www/cgi-bin/gettypes.py
@@ -3,8 +3,23 @@
 print "Content-Type: text/html"
 print # don't forget the extra newline	
 
+import sqlite3
+conn = sqlite3.connect('donors.db')
+c = conn.cursor()
+
 import json
 data = {'name':'Kasun', 'address':'columbo','age': '29'}
 data['myName'] = 'Jaime'
 data['myLastName'] = 'Montoya'
+
+counter = 0
+for r in c.execute('SELECT typeid, typename FROM type;'):
+  counter += 1
+  #data[counter] = r['typeid']
+  # r[0] corresponds to typeid. r[1] corresponds to typename.
+  data[r[0]] = r[1]
+
+
+
+
 print json.dumps(data)

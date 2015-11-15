94a39c3649da8331118905093f9510ac938d9ec5
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-14 21:34:19 -0500

    Update gettypes.py

--- www/cgi-bin/gettypes.py
+++ www/cgi-bin/gettypes.py
@@ -8,14 +8,12 @@ conn = sqlite3.connect('donors.db')
 c = conn.cursor()
 
 import json
-data = {'name':'Kasun', 'address':'columbo','age': '29'}
-data['myName'] = 'Jaime'
-data['myLastName'] = 'Montoya'
+data = {}
+#data = {'name':'Kasun', 'address':'columbo','age': '29'}
+#data['myName'] = 'Jaime'
+#data['myLastName'] = 'Montoya'
 
-counter = 0
 for r in c.execute('SELECT typeid, typename FROM type;'):
-  counter += 1
-  #data[counter] = r['typeid']
   # r[0] corresponds to typeid. r[1] corresponds to typename.
   data[r[0]] = r[1]
 

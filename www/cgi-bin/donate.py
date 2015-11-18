554552ec2bfcf759014433d72c4bef81f9d9f0cc
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-18 12:38:03 -0500

    Support for multiple users and sessions

--- www/cgi-bin/donate.py
+++ www/cgi-bin/donate.py
@@ -8,6 +8,7 @@ print # don't forget the extra newline
 
 import cgi
 form = cgi.FieldStorage()
+usernameVal = form['usernameValue'].value
 itemidVal = form['itemidValue'].value
 descriptionVal = form['descriptionValue'].value
 quantityVal = form['quantityValue'].value
@@ -19,7 +20,7 @@ c = conn.cursor()
 import json
 
 data = {}
-c.execute('INSERT INTO user_donation (username, itemid, description, quantity) VALUES (?, ?, ?, ?)', ('jaimemontoya', itemidVal, descriptionVal, quantityVal))
+c.execute('INSERT INTO user_donation (username, itemid, description, quantity) VALUES (?, ?, ?, ?)', (usernameVal, itemidVal, descriptionVal, quantityVal))
 data = "Donation completed!"
 
 conn.commit()

71952bda6682303479a0081bc917066c1a50ae4b
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-18 12:43:14 -0500

    Create getsubtypes.py

---
+++ www/cgi-bin/getsubtypes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+print "Content-Type: text/html"
+print # don't forget the extra newline	
+
+import cgi
+form = cgi.FieldStorage()
+
+obtainedType = form['selecttypeValue'].value
+
+import sqlite3
+conn = sqlite3.connect('donors.db')
+c = conn.cursor()
+
+import json
+data = {}
+#data['mykey'] = 'This is working and the selecttypeValue is:' + obtainedType
+
+for r in c.execute('SELECT itemid, itemname FROM item WHERE itemtypeid=?;',[obtainedType]):
+  # r[0] corresponds to itemid. r[1] corresponds to itemname.
+  data[r[0]] = r[1]
+
+#data = {'name':'Kasun', 'address':'columbo','age': '29'}
+#data['myName'] = 'Jaime'
+#data['myLastName'] = 'Montoya'
+
+#counter = 0
+#for r in c.execute('SELECT typeid, typename FROM type;'):
+  #counter += 1
+  #data[counter] = r['typeid']
+  # r[0] corresponds to typeid. r[1] corresponds to typename.
+  #data[r[0]] = r[1]
+
+
+
+
+print json.dumps(data)
